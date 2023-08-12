03d522e3156ba7e20676935a5d6fd6398933637b
Kevin Beliën <k3vin.belien@gmail.com>
2023-08-12 21:41:59 +0200

    fzat: setup component library and service

--- projects/chit-chat/src/lib/chit-chat.module.ts
+++ projects/chit-chat/src/lib/chit-chat.module.ts
@@ -1,16 +1,40 @@
-import { NgModule } from '@angular/core';
+
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { InjectionToken, ModuleWithProviders, NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 import { ChitChatComponent } from './chit-chat.component';
+import { ChatService } from './services';
 
+export interface LibConfig {
 
+}
+
+export const LibConfigService = new InjectionToken<LibConfig>('LibConfig');
 
 @NgModule({
   declarations: [
     ChitChatComponent
   ],
-  imports: [
+imports: [
+    CommonModule,
+    HttpClientModule,
+    IonicModule
   ],
   exports: [
     ChitChatComponent
   ]
 })
-export class ChitChatModule { }
+export class ChitChatModule {
+  static forRoot(config: LibConfig): ModuleWithProviders<ChitChatModule> {
+    return {
+      ngModule: ChitChatModule,
+      providers: [
+        ChatService,
+        {
+          provide: LibConfigService,
+          useValue: config
+        }
+      ]
+    };
+  } }
---
+++ projects/chit-chat/src/lib/services/chat.service.ts
@@ -0,0 +1,11 @@
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { LibConfig, LibConfigService } from '../chit-chat.module';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+
+  constructor(@Inject(LibConfigService) private config: LibConfig, private http: HttpClient) { }
+}
