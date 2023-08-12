c7e5f7eb2b76c091c2767fc47c8d29b731eedbc1
Kevin Beliën <k3vin.belien@gmail.com>
2023-08-12 21:43:19 +0200

    fix: public api service export

--- projects/chit-chat/src/public-api.ts
+++ projects/chit-chat/src/public-api.ts
@@ -2,6 +2,7 @@
  * Public API Surface of chit-chat
  */
 
-export * from './lib/chit-chat.service';
 export * from './lib/chit-chat.component';
 export * from './lib/chit-chat.module';
+export * from './lib/services/';
+
