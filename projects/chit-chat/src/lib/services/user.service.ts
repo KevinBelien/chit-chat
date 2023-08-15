7b9c568b99f048cc5f8dda5859f177c9e63c4493
Kevin Beliën <k3vin.belien@gmail.com>
2023-08-15 21:53:12 +0200

    feat: create user to firebase

--- projects/chit-chat/src/lib/services/user.service.ts
+++ projects/chit-chat/src/lib/services/user.service.ts
@@ -1,6 +1,13 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import {
+	collection,
+	doc,
+	getFirestore,
+	setDoc,
+} from 'firebase/firestore';
 import { map } from 'rxjs';
+import { LibConfig, LibConfigService } from '../chit-chat.module';
 import { FireStoreCollection } from '../enums';
 import { User } from '../models';
 import { UserStatus } from './../types/user-status.type';
@@ -9,7 +16,22 @@ import { UserStatus } from './../types/user-status.type';
 	providedIn: 'root',
 })
 export class UserService {
-	constructor(private afs: AngularFirestore) {}
+	constructor(
+		@Inject(LibConfigService) private config: LibConfig,
+		private afs: AngularFirestore
+	) {
+		// this.createUser(
+		// 	new User(
+		// 		'Test-1234',
+		// 		'Test again',
+		// 		new UserRole('test-role', 'test role', Date.now(), []),
+		// 		new Date(),
+		// 		null,
+		// 		'available',
+		// 		true
+		// 	)
+		// );
+	}
 
 	getUsers = () => {
 		return this.afs
@@ -33,5 +55,21 @@ export class UserService {
 			.collection(FireStoreCollection.USERS)
 			.doc(documentID)
 			.update({ onlineStatus: status });
+	};
+
+	createUser = async (data: User): Promise<void> => {
+		const fireStore = getFirestore();
+		const usersRef = collection(fireStore, FireStoreCollection.USERS);
+
+		try {
+			return await setDoc(
+				doc(usersRef, data.uid),
+				JSON.parse(JSON.stringify(data))
+			);
+		} catch (e: any) {
+			throw Error(
+				`Something went wrong when creating user with uid ${data.uid}. ${e}`
+			);
+		}
 	};
 }
