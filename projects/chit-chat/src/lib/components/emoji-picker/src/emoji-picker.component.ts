919299f75da4ddf88057d03deb06c0cc653a7799
Kevin Beliën <k3vin.belien@gmail.com>
2024-05-25 10:59:17 +0200

    feat: components generated

---
+++ projects/chit-chat/src/lib/components/emoji-picker/src/emoji-picker.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ch-emoji-picker',
+  standalone: true,
+  imports: [],
+  templateUrl: './emoji-picker.component.html',
+  styleUrl: './emoji-picker.component.scss'
+})
+export class EmojiPickerComponent {
+
+}
